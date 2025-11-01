3de329872887879a3a73348e5ba7200940e1eada
nihat <nihtavci@gmail.com>
2025-11-01 03:28:00 +0100

    fix: remove unnecessary UI elements and fix social sidebar visibility
    - Remove star icon from article metadata
    - Remove Facebook/Twitter/Instagram social links from author box
    - Center author metadata with flex layout
    - Fix social sticky sidebar by changing left position from -20% to -80px
    - Fix author avatar alignment in centered metadata

--- components/sections/single/Section1.tsx
+++ components/sections/single/Section1.tsx
@@ -68,11 +68,6 @@ export default function Section1({ article }: Section1Props) {
               <span className="readingTime" title={article.readTime}>
                 {article.readTime}
               </span>
-              <span className="svgIcon svgIcon--star">
-                <svg className="svgIcon-use" width={15} height={15}>
-                  <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
-                </svg>
-              </span>
             </div>
           </div>
         </header>
@@ -225,17 +220,6 @@ export default function Section1({ article }: Section1Props) {
                   </h5>
                 </div>
                 <p className="d-none d-md-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet ut ligula et semper. Aenean consectetur, est id gravida venenatis.</p>
-                <div className="content-social-author">
-                  <Link target="_blank" className="author-social" href="#">
-                    Facebook
-                  </Link>
-                  <Link target="_blank" className="author-social" href="#">
-                    Twitter
-                  </Link>
-                  <Link target="_blank" className="author-social" href="#">
-                    Instagram
-                  </Link>
-                </div>
               </div>
             </div>
           </div>
