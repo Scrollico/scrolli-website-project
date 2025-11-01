0bb0f4b575ea51428bb0ddbd8db1251eb7e7e4b6
nihat <nihtavci@gmail.com>
2025-11-01 03:40:05 +0100

    fix: improve related posts entry-meta layout
    - Update related posts swiper to use entry-meta-line containers
    - Remove br tags and use proper flex layout for better spacing
    - Prevents metadata overflow in swiper items

--- components/sections/single/Section1.tsx
+++ components/sections/single/Section1.tsx
@@ -281,18 +281,16 @@ export default function Section1({ article }: Section1Props) {
                           <Link href={`/article/${post.id}`}>{post.title}</Link>
                         </h5>
                         <div className="entry-meta align-items-center">
-                          <Link href="/author">{post.author}</Link> in <Link href="/archive">{post.category}</Link>
-                          <br />
-                          <span>{post.date}</span>
-                          <span className="middotDivider" />
-                          <span className="readingTime" title={post.readTime}>
-                            {post.readTime}
-                          </span>
-                          <span className="svgIcon svgIcon--star">
-                            <svg className="svgIcon-use" width={15} height={15}>
-                              <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
-                            </svg>
-                          </span>
+                          <div className="entry-meta-line">
+                            <Link href="/author">{post.author}</Link> in <Link href="/archive">{post.category}</Link>
+                          </div>
+                          <div className="entry-meta-line">
+                            <span>{post.date}</span>
+                            <span className="middotDivider" />
+                            <span className="readingTime" title={post.readTime}>
+                              {post.readTime}
+                            </span>
+                          </div>
                         </div>
                       </div>
                     </div>
